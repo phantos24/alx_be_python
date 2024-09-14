e0073b95970cf376d0790e69f1f92f0cc0dbfb9d
phantos <mahmoudgamalsad@gmail.com>
2024-09-14 04:55:04 -0700

    add new files

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+add = number1 + number2
+sub = number1 - number2
+multi = number1 * number2
+
+print("Addition of %d and %d is %d " % (number1 ,number2 ,add))
+print("Subtraction of %d and %d is %d " % (number1 ,number2 ,sub))
+print("Multiplication of %d and %d is %d " % (number1 ,number2 ,multi))
