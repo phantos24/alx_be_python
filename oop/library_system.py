d78424fdce19c55860cb17e4edcd9c2e9b23c2ed
phantos <mahmoudgamalsad@gmail.com>
2024-10-13 12:52:05 -0700

    update

--- oop/library_system.py
+++ oop/library_system.py
@@ -33,5 +33,5 @@ class Library():
 
     def list_books(self):
         for x in range(len(self.books)):
-            print (f"{type(x).__name}: {self.books[x]}")
+            print (f'{type(self.books[x]).__name__}: {self.books[x]}')
 
