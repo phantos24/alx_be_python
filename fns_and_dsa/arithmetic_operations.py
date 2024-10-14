5b99f3f00df957d8f5736f2774196ae1bd8e1ad2
phantos <mahmoudgamalsad@gmail.com>
2024-10-14 16:07:20 -0700

    editing arithmetic_operation.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,5 +9,5 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 print("can't divie by zero.")
-            else:
+            elif num2 != 0:
                 return num1 / num2
