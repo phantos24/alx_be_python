e2af1dcbcb0a0afbc41c43d2a1b0745b2b5eec87
phantos <mahmoudgamalsad@gmail.com>
2024-10-16 03:12:31 -0700

    adding temp_conversion_tool.py

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,34 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+def convert_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+
+def convert_to_fahrenheit(celsius):
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+
+
+
+while (True):
+    global degree
+    try:
+        degree = float(input("Enter the temperature to convert: "))
+        break
+    except:
+         print("please enter a vaild degree: ")
+
+       
+
+while (True):
+    temp = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").lower()
+    match (temp):
+        case "c":
+            converted = convert_to_fahrenheit(degree)
+            print(f"{degree}°C is {converted}°F")
+            break
+        case "f":
+            converted = convert_to_celsius(degree)
+            print(f"{degree}°F is {converted}°C")
+            break
+        case _:
+            print("Please enter a vaild temprature.")
