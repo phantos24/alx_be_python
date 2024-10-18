f5cc6a36aa7f3fee8c21c1ef8ae7e2b294aa8fc1
phantos <mahmoudgamalsad@gmail.com>
2024-10-18 05:59:16 -0700

    adding test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,18 +5,18 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
        self.calc = SimpleCalculator()
     
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(10, 200), 210)
 
-    def test_sub(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(2, 1), 1)
         self.assertEqual(self.calc.subtract(0, 1), -1)
         self.assertEqual(self.calc.subtract(200, 50), 150)
         self.assertEqual(self.calc.subtract(200, -50), 250)
 
-    def test_multi(self):
+    def test_multiply(self):
         self.assertEqual(self.calc.multiply(2, 5), 10)
         self.assertEqual(self.calc.multiply(10, 5), 50)
         self.assertEqual(self.calc.multiply(2, -3), -6)
