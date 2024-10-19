979cdbfab356c48064b2bb0fcd25736578789854
phantos <mahmoudgamalsad@gmail.com>
2024-10-19 08:56:24 -0700

    adding library_management.py

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -11,16 +11,16 @@ class Book:
 
 class Library:
     def __init__(self):
-        self.__books = []
+        self._books = []
     
     def add_book(self, book):
         if isinstance(book, Book):
-            self.__books.append(book)
+            self._books.append(book)
         else:
             print("Only Book instances can be added.")
 
     def check_out_book(self, title):
-        for book in self.__books:
+        for book in self._books:
             if (book.title == title and book._is_checked_out == False):
                 book._is_checked_out = True
                 print(f"{title} checked out")
@@ -29,7 +29,7 @@ class Library:
             print(f"{title} is not available")
 
     def return_book(self, title):
-        for book in self.__books:
+        for book in self._books:
             if (book.title == title and book._is_checked_out == True):
                 book.return_book()
                 print(f"{title} Returned")
@@ -38,9 +38,6 @@ class Library:
             print(f"{title} is not not checked out")
 
     def list_available_books(self):
-            for book in self.__books:
+            for book in self._books:
                 if (book._is_checked_out == False):
                     print(f"{book.title} by {book.author}")
-
-
-    
